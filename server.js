2baadd04abdd4f986f41ef82d31e0e35b721550b
tharamesseroux <messeroux.t@northeastern.edu>
2022-04-10 17:30:16 -0400

    a8 MONGO sp22

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 import cors from 'cors';
+// import tuitsController
+//   from "./tuits/tuits-controller.js";
+// load the mongoose library
+// connect to the webdev database
+
 
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(express.json());
 app.use(cors());
