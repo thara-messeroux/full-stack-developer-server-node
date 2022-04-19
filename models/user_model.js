607143e63a8363fe73623c99d89d128ccd8a2287
tharamesseroux <messeroux.t@northeastern.edu>
2022-04-19 18:59:06 -0400

    register

---
+++ models/user_model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const User =  new mongoose.Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+}, {collection: 'user-data'})
+
+const model = mongoose.model("UserData", User);
+module.exports = model
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import mongoose from "mongoose";
+
+const User = require('./models/user_model')
 const DB_CONNECTION_STRING = 'mongodb+srv://tharamesseroux:braTtitude17*@cluster0.xssrp.mongodb.net/webdev?retryWrites=true&w=majority';
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev'
 mongoose.connect(CONNECTION_STRING);
@@ -20,12 +22,29 @@ app.use(cors());
 helloController(app);
 userController(app);
 tuitsController(app);
-app.listen(process.env.PORT || 4000, () => {
-  console.log("Welcome to backend development");
-});
 
 app.get('/', (req,res) => {
   res.send("Welcome to backend development")
 })
+// asynchronous
+app.post('/api/register', async (req, res) => {
+  try{
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    })
+    res.send(user)
+  }
+    catch(e){
+      res.status(400).send(e)
+    }
+})
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log("Welcome to backend development");
+});
+
+
 // import controller
 // pass it app
